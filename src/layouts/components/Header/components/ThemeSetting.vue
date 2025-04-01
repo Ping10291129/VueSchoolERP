63e4bdb0d8aac14e712a32573a814df617ce40da
A330-zyx <zyx10291129@gmail.com>
2025-04-01 10:22:35 +0800

    refactor(Header): 替换主题设置图标为Element Plus的Setting组件
    将主题设置图标从iconfont替换为Element Plus的Setting组件，以统一UI组件库的使用并提高代码的可维护性。

--- src/layouts/components/Header/components/ThemeSetting.vue
+++ src/layouts/components/Header/components/ThemeSetting.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="theme-setting">
-    <i :class="'iconfont icon-zhuti'" class="toolBar-icon" @click="openDrawer"></i>
+    <el-icon class="toolBar-icon iconfont" @click="openDrawer">
+      <Setting />
+    </el-icon>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Setting } from "@element-plus/icons-vue";
 import mittBus from "@/utils/mittBus";
 const openDrawer = () => {
   mittBus.emit("openThemeDrawer");
 };
 </script>
+
+<style scoped lang="scss">
+.iconfont {
+  height: 24px;
+  font-family: iconfont !important;
+  font-size: 22px;
+  font-style: normal;
+  cursor: pointer;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
